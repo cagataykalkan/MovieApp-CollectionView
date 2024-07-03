760afa2b6d58f79f861c54b23c4e359cfc5f76f6
cagataykalkan <c.kalkan203@gmail.com>
2024-07-03 14:33:29 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Çağatay KALKAN on 3.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
