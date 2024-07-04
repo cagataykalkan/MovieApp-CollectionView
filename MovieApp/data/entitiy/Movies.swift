a2283689cb36c191c344d3bc37b4d4238f0df464
cagataykalkan <c.kalkan203@gmail.com>
2024-07-04 16:14:31 +0300

    commit1

---
+++ MovieApp/data/entitiy/Movies.swift
@@ -0,0 +1,8 @@
+//
+//  Movies.swift
+//  MovieApp
+//
+//  Created by Çağatay KALKAN on 3.07.2024.
+//
+
+import Foundation
---
+++ MovieApp/ui/tableview/MovieCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by Çağatay KALKAN on 4.07.2024.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
